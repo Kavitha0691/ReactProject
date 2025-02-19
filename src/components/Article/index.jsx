e850be3428b3ab30e9c7fcae326904efa9083406
kavitha0691 <kavitha0691@gmail.com>
2025-02-19 10:08:39 +0100

    added img

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -1,8 +1,10 @@
 import styles from './article.module.css'
+import Flower from '../../assets/flower.jpeg'
 
 const Article = () => {
     return (
       <article className = {styles.article}>
+        <img src={Flower} alt="flower" />
        <h2>Article</h2>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloremque quo molestiae ab quae hic quibusdam, dolorum inventore sunt fugit! Hic et earum quis. Quibusdam ex ad tenetur impedit dolore?</p>
       </article>
